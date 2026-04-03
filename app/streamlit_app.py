324ed8a6d250609b87a2566ffeeedbe161189675
Prachya Suman Das <prachyasuman2396@gmail.com>
2026-04-03 15:25:28 +0530

    feat: fixed Streamlit API integration and error handling

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 import pandas as pd
-import joblib
-import pandas as pd
-import sys
-import os
 
-sys.path.append(os.path.abspath("."))
+import requests
 
-
-model = joblib.load("artifacts/model.pkl")
+api_url= 'https://delivery-time-prediction-ml-1.onrender.com/predict'
 
 st.set_page_config(page_title="Delivery Time Predictor", layout="centered")
 
@@ -45,7 +40,7 @@ experience = st.number_input("Courier Experience (years)", min_value=0.0, value=
 
 if st.button("Predict Delivery Time"):
 
-    input_data = pd.DataFrame([{
+    input_data = {
         "Distance_km": distance,
         "Weather": weather,
         "Traffic_Level": traffic,
@@ -53,8 +48,18 @@ if st.button("Predict Delivery Time"):
         "Vehicle_Type": vehicle,
         "Preparation_Time_min": prep_time,
         "Courier_Experience_yrs": experience
-    }])
+    }
 
-    prediction = model.predict(input_data)[0]
+    try:
+        response = requests.post(api_url, json=input_data)
 
-    st.success(f"Estimated Delivery Time: {round(prediction, 2)} minutes")
+        if response.status_code == 200:
+            result = response.json()
+            prediction = result['predicted_delivery_time']
+            st.success(f"Estimated Delivery Time: {round(prediction, 2)} minutes")
+
+        else:
+            st.error(f"API Error: {response.json()}")
+
+    except Exception as e:
+        st.error(f"Request failed: {str(e)}")
